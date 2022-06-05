5478d6138ccb3219c38dc21ea1c45ffd43ffd630
Comot819 <106909663+Comot819@users.noreply.github.com>
2022-06-05 18:12:03 +0800

    Add files via upload

---
+++ data/Medan_Selera2_3.js
@@ -0,0 +1,8 @@
+var json_Medan_Selera2_3 = {
+"type": "FeatureCollection",
+"name": "Medan_Selera2_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "building": "Food_Court", "Location": "Medan Selera Merlimau Point" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 102.430394370107635, 2.164962560061355 ], [ 102.430659102063089, 2.164944107012693 ], [ 102.430656615239229, 2.164308155274211 ], [ 102.43036906929116, 2.164351870154948 ], [ 102.430394370107635, 2.164962560061355 ] ] ] ] } }
+]
+}
---
+++ data/Mosque_4.js
@@ -0,0 +1,8 @@
+var json_Mosque_4 = {
+"type": "FeatureCollection",
+"name": "Mosque_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Building": null, "Location": "Masjid As Syujaah Merlimau", "Religious": "Muslim" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 102.431029251899233, 2.166372092483862 ], [ 102.431418927163591, 2.166277877341102 ], [ 102.431322185137702, 2.165983099793915 ], [ 102.430948100180359, 2.166083582348288 ], [ 102.431029251899233, 2.166372092483862 ] ] ] ] } }
+]
+}
